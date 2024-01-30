f447db7344b5910b0c0d390b27a9fd89445b3418
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-01-30 11:45:32 +0300

    add user model

---
+++ scp/models.py
@@ -0,0 +1,14 @@
+from flask_login import UserMixin
+from scp import login_manager, db
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.gt(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
