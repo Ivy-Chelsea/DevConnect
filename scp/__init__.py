40d85cf1ae398200b8e747976b31920c74a0ce23
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-01-30 11:44:21 +0300

    add database, login manager and configurations

--- scp/__init__.py
+++ scp/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '3ae5979e750707f3ba7ff616f2428731'
+app.config['SECRET_KEY'] = ''
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
+login_manager = LoginManager(app)
 
 from scp import routes
