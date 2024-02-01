dedeee8ef0c47b5f82df7165d5b0cfafb338f3c2
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-02-01 23:15:41 +0300

    import and initialize bcrypt for password hashing

--- scp/__init__.py
+++ scp/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
@@ -8,5 +9,6 @@ app.config['SECRET_KEY'] = ''
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
+bcrypt = Bcrypt(app)
 
 from scp import routes
