9f2c71ca611dedaba2ab6c1872952c80431e793c
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-02-01 23:35:47 +0300

    remove/add whitespaces where necessary

--- scp/routes.py
+++ scp/routes.py
@@ -23,13 +23,14 @@ def signup():
     form = SignupForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email = form.email.data, password = hashed_password)
+        user = User(username=form.username.data,
+                    email=form.email.data,
+                    password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash(f'Account created for {form.username.data}', 'success')
         return redirect(url_for('home'))
     return render_template('signup.html', title='Signup', form=form)
-
 
 
 app.route("/login", methods=['GET', 'POST'])
