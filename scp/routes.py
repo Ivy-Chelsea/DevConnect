df4d1f6b276044e9ff91be1d74781040c7ccfe2b
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-02-01 23:25:43 +0300

    import from form.py & model.py for user authentication when signing up

--- scp/routes.py
+++ scp/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, url_for, redirect
-from scp import app
+from scp import app, bcrypt
 from flask_login import current_user
+from scp.form import *
+from scp.models import *
 
 
 @app.route("/")
@@ -18,6 +20,11 @@ app.route("/signup", methods=['GET', 'POST'])
 def signup():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
+    form = SignupForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email = form.email.data, password = hashed_password)
+
 
 
 app.route("/login", methods=['GET', 'POST'])
