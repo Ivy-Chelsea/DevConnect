c17963bf7c61dd3aa747432e0b38c62bb5cd41f2
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-02-01 23:27:28 +0300

    commit newly added user

--- scp/routes.py
+++ scp/routes.py
@@ -25,6 +25,7 @@ def signup():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, email = form.email.data, password = hashed_password)
         db.session.add(user)
+        db.session.commit()
 
 
 
