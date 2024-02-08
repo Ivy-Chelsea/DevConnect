112270c416c0d96cc6aeef5ab3c1d360bb54f11c
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-02-08 19:48:09 +0300

    import User from models for username validation

--- scp/form.py
+++ scp/form.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from scp.models import User
 
 
 class SignupForm(FlaskForm):
