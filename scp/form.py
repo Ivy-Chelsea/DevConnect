fe6d56ec29d85e7cff869df1c22fa8f89ea31111
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-02-08 19:42:57 +0300

    create post form

--- scp/form.py
+++ scp/form.py
@@ -22,3 +22,9 @@ class SignupForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('Email already exists, try a different one!')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
