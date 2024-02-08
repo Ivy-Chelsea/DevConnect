bdd99b397dcb036ac0dd23f38be070526e0e708e
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-02-08 19:43:51 +0300

    import TextAreaField for the post form

--- scp/form.py
+++ scp/form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 
