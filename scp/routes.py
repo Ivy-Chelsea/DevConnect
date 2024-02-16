49b33e018f853e5587c2f6f9ff639acc574a78b0
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-02-16 14:39:50 +0300

    redirect user to page they were trying to access before login

--- scp/routes.py
+++ scp/routes.py
@@ -42,3 +42,5 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password):
             login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
