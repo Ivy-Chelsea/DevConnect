3bb8866f23662e7dfc57354e53074c6a736121b9
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-02-16 14:12:27 +0300

    import boolean field from wtforms

--- scp/form.py
+++ scp/form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from scp.models import User
 
