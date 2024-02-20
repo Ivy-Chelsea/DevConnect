0806605f27aaad4a8b700a842d22c26fb8ddc72e
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-02-20 18:37:14 +0300

    add () to make it an instance

--- scp/routes.py
+++ scp/routes.py
@@ -37,7 +37,7 @@ def signup():
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
-    form = LoginForm
+    form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password):
