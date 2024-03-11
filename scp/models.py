3cf5eb131df09f0b047c67eb0ba1fceb668f7843
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-03-11 22:10:44 +0300

    add image in database to allow users to set profiel pictures

--- scp/models.py
+++ scp/models.py
@@ -13,6 +13,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    image = db.Column(db.String(20), nullable=False, default='default.jpg')
 
 
 class Post(db.Model):
