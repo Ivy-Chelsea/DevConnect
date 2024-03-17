d733bc63c4faf5fdf818ad354b2bf752ef2f395b
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-03-17 23:36:27 +0300

    join random characters generated to create a code string

--- scp/gen.py
+++ scp/gen.py
@@ -1,4 +1,4 @@
 def generate_room_code(length: int, existing_code):
     while True:
         code_chars = [random.choice(ascii_letters) for _ in range(length)]
-
+        code = ''.join(code_chars)
