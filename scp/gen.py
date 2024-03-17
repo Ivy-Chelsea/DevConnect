43b95a8c95a0763882991e3301486fdab8a18afc
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-03-17 23:37:35 +0300

    return the code generated if it's not already in existing codes

--- scp/gen.py
+++ scp/gen.py
@@ -2,3 +2,6 @@ def generate_room_code(length: int, existing_code):
     while True:
         code_chars = [random.choice(ascii_letters) for _ in range(length)]
         code = ''.join(code_chars)
+        if code not in existing_code:
+            return code
+
