7fdfd644c070217c08252931639f35c0373031dc
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-03-17 23:39:58 +0300

    import dependencies that'll help with random character development

--- scp/gen.py
+++ scp/gen.py
@@ -1,3 +1,7 @@
+import random
+from string import ascii_letters
+
+
 def generate_room_code(length: int, existing_code):
     while True:
         code_chars = [random.choice(ascii_letters) for _ in range(length)]
