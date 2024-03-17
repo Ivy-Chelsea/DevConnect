97ca36d3be79448eca2357bd8686379d991bc818
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-03-17 23:52:02 +0300

    import socketio to help with real time communication

--- scp/__init__.py
+++ scp/__init__.py
@@ -2,11 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from flask_socketio import SocketIO
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = ''
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 bcrypt = Bcrypt(app)
