407e9475ba08b7f55e887e2a18e71d228623fd0c
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-04-05 22:06:43 +0300

    function to send a reset message to user requesting password reset

--- scp/gen.py
+++ scp/gen.py
@@ -8,3 +8,16 @@ def generate_room_code(length: int, existing_code):
         code = ''.join(code_chars)
         if code not in existing_code:
             return code
+
+
+def reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request',
+                  sender='sscarlette84@gmail.com',
+                  recipients=[user.email])
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('reset_token', token=token, _external=True)}
+
+If you did not make this request then simply ignore this email and no changes will be made.
+'''
+    mail.send(msg)
