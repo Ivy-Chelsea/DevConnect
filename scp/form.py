a717a160056b055a8b8e2ba18f9c8c608de98724
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-04-05 21:59:59 +0300

    create a form for reseting a password

--- scp/form.py
+++ scp/form.py
@@ -74,3 +74,10 @@ class RequestResetForm(FlaskForm):
     email = StringField('email',
                         validators=[DataRequired(), Email()])
     submit = SubmitField('Request Password Reset')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
